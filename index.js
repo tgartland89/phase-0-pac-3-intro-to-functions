d17073b3e1d68c220b3caeca52812ad80f969ef6
tgartland89 <thomas.gartland@gmail.com>
2023-04-13 16:51:34 -0600

    function revisit

--- index.js
+++ index.js
@@ -1,39 +1,29 @@
-function sayHelloToGuadalupe() {
-    console.log("Hello, Guadalupe!");
-  }
-  sayHelloToGuadalupe();
-  function sayHelloToLiz() {
-    console.log("Hello, Liz!");
-  }
-  sayHelloToLiz();
-  function sayHelloToSamip() {
-    console.log("Hello, Samip!");
-  }
-  sayHelloToSamip();
-  
-  sayHelloToGuadalupe();
-  sayHelloToLiz()
-  sayHelloToSamip()
-  function say(greeting, firstName) {
-    console.log(`${greeting}, ${firstName}!`);
-  }
-  say("Julio" , "Hello")
-  function say(greeting, firstName) {
-    console.log("firstName: ", firstName);
-    console.log("greeting: ", greeting);
-    console.log(`${greeting}, ${firstName}!`);
-  }
-  function add(x, y) {
-    return x + y;
-  }
-  add(1,2)
-  console.log(add(1,2))
-  function say(greeting, firstName) {
-    console.log(`${greeting}, ${firstName}!`);
-  }
-  console.log(say("Hello", "Liz"));
-  function say(greeting, firstName) {
-    console.log("I was called!");
-    return `${greeting}, ${firstName}!`;
-  }
-  console.log(say("Howdy", "partner"));
+function sayHello() {
+  console.log("Hello!")
+}
+function sayHelloToGuadalupe(){
+  console.log("Hello, Guadalupe!")
+}
+function sayHelloToLiz(){
+  console.log("Hello, Liz!")
+}
+function sayHelloToSamip(){
+  console.log("Hello, Samip!")
+}
+
+function sayHelloTo(firstName){
+  console.log(`Hello, ${firstName}!`)
+}
+function say(greeting, firstName){
+  console.log(`${greeting}, ${firstName}!`)
+}
+function add(x,y){
+  return x + y
+}
+console.log(say("Hello" ,"Liz"))
+console.log(add(1,2))
+function say(greeting, firstName) {
+  console.log("I was called!")
+  return `${greeting}, ${firstName}!`
+}
+console.log(say("Howdy", "partner"))
